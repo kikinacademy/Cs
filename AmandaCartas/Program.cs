490d008bbc3fd33025ce6366db373baa5bb1180b
KikinAcademy <103606817+kikinacademy@users.noreply.github.com>
2022-06-21 16:23:44 -0500

    Add files via upload

---
+++ AmandaCartas/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace AmandaCartas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var fig = Menu_figura();
+            Console.Clear();
+            var n = Aleatorio();
+            var n_oculto = Aleatorio();
+            Carta_volteada(20, 1);
+            Carta(20, 8, n, fig);
+            var respuesta = Pregunta();
+            Console.Clear();
+            Carta(20, 1, n_oculto, fig);
+            Carta(20, 8, n, fig);
+            Verificar(respuesta, n, n_oculto);
+        }
+
+        private static void Carta(int p0, int p1, string p2, string fig)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Carta_volteada(int i, int i1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Verificar(string respuesta, string s, string nOculto)
+        {
+            var num1 = int.Parse(s);
+            var num2 = int.Parse(nOculto);
+            if (respuesta == "up") Console.WriteLine(num2 > num1 ? "Correcto" : "Incorrecto");
+            if (respuesta == "down") Console.WriteLine(num2 < num1 ? "Correcto" : "Incorrecto");
+        }
+
+        private static string Pregunta()
+        {
+            Console.WriteLine("¿Será la carta mayor o menor?");
+            var mensaje = "Presiona la tecla hacia arriba para mayor o hacia abajo para menor";
+            var respuesta = Tecla_Presionada(mensaje);
+            return respuesta;
+        }
+
+        private static string Tecla_Presionada(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+            return Console.ReadKey().Key.ToString();
+        }
+
+        private static string Aleatorio()
+        {
+            var rnd = new Random();
+            var n = rnd.Next(1, 10);
+            return n.ToString();
+        }
+
+        private static string Menu_figura()
+        {
+            string symbol;
+            Console.WriteLine("Bienvenido al juego de cartas *¿mayor o menor?*");
+            Console.WriteLine("REGLAS:");
+            Console.WriteLine("En pantalla apareceran 2 cartas. 1 Boca abierto y otra boca abajo");
+            Console.WriteLine("El usuario presionará arriba cuando crea que la carta boca abajo es superior");
+            Console.WriteLine("El usuario presionará abajo cuando crea que la carta boca abajo es inferior");
+            Console.WriteLine(
+                "Seleciona la figura de la baraja que te gustaría usar, indicando el inciso correspondiente: ");
+            Console.WriteLine("a) ♥");
+            Console.WriteLine("b) ♦");
+            Console.WriteLine("c) ♣");
+            Console.WriteLine("d) ♠");
+            symbol = Console.ReadLine();
+            return symbol;
+        }
+    }
+}
---
+++ CS-Ricardo-Algoritmos-LogicaProgramacion/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CS_Ricardo_Algoritmos_LogicaProgramacion
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Arreglo de 5 numeros con un decimal
+            var arreglo = new decimal[5];
+
+            // Pide cada uno de los elementos del arreglo
+            for (var i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write("Ingrese el numero {0}: ", i + 1);
+                arreglo[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            // Invocar al método ordenar
+            ordenar(ref arreglo);
+
+            // Preguntar otro numero decimal
+            Console.Write("\n\nIngrese otro numero decimal: ");
+            var numero = Convert.ToDecimal(Console.ReadLine());
+
+            // Informar si se encuentra en el arreglo y en que posicion está
+            var encontrado = false;
+            var posicion = 0;
+            for (var i = 0; i < arreglo.Length; i++)
+                if (arreglo[i] == numero)
+                {
+                    encontrado = true;
+                    posicion = i;
+                    Console.WriteLine("\n");
+                    Console.WriteLine("El numero {0} se encuentra en la posicion {1}", numero, posicion + 1);
+                    break;
+                }
+
+            Console.WriteLine("\n");
+            if (!encontrado) Console.WriteLine("El numero {0} no se encuentra en el arreglo", numero);
+
+            // Imprime el arreglo
+            Console.WriteLine("El arreglo ya ordenado es: ");
+            for (var i = 0; i < arreglo.Length; i++) Console.Write("{0}", arreglo[i] + " , ");
+        }
+
+        private static void ordenar(ref decimal[] arreglo)
+        {
+            // Ordenar de menor a mayor con ordenamiento burbuja
+            for (var i = 0; i < arreglo.Length; i++)
+            for (var j = 0; j < arreglo.Length - 1; j++)
+                if (arreglo[j] > arreglo[j + 1])
+                {
+                    // Con variable auxiliar para intercambiar
+                    var aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+
+                    // Con Swap
+                    //(arreglo[j], arreglo[j + 1]) = (arreglo[j + 1], arreglo[j]);
+                }
+        }
+    }
+}
